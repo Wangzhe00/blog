d5174b211d68ee2b777eb5dfa792d785648c9925
Wangzhe00 <wangzhe8889@163.com>
2022-04-21 17:36:04 +0800

    test: baiduPush

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://b.wangzhee.cn&token=d7Ax66Sp6SgDC4Dp"
 
 rm -rf urls.txt # 删除文件

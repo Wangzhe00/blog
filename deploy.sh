e2eb55613b6f9bdc7b51f3e70f6533ba8c9b4aec
Wangzhe00 <wangzhe8889@163.com>
2022-05-11 20:38:47 +0800

    test: fill-in-keys.sh env

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,23 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+id=("BAIDU_PUSH_TOKEN" "GITTALK_COMMENT_CLIENTID" "GITTALK_COMMENT_CLIENT_SECRET")
+
+dirs=("baiduPush.sh" "./docs/.vuepress/config.ts" "./docs/.vuepress/config.ts")
+
+len=${#id[*]}
+
+for (( i=0; i<$len; i=i+1 )); do
+    t=${id[i]}
+    if [ -n "${!t}" ]; then
+        sed -i "s/${id[i]}/${!t}/g" ${dirs[i]}
+        echo "success fill in $t in ${dirs[i]}"
+    else
+        echo "can't find $t env or ${dirs[i]} dir"
+        exit 1
+    fi
+done
+
 ./fill-in-keys.sh
 
 # 生成静态文件
